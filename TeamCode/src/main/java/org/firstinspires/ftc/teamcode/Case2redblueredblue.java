f974f78b1539dc994c93eda63778ba110450bfd5
szost <tszostak.2019@centrenord.ab.ca>
2017-02-10 23:59:38 -0700

    Hey guys it's Theo! I made these files just in case the camera detection dosen't work at the competition. These are 4 files that should work for every beacon situation. They are as followed: Case 1: Beacon 1 and Beacon 2 = left light blue and right light red Case 2: Beacon 1 and Beacon 2 = left light red and right light blue Case 3: Beacon 1 = left light blue and right light red where as beacon 2 = left light red and right light blue Case 4: Beacon 1 = left light red and right light blue where as beacon 2 = left light blue and right light red.
     These cases are programmed for each alliance (red and blue)
     Goodnight see you both tommorow at the competition!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Case2redblueredblue.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by szost on 2017-02-10.
+ */
+
+final class Case2redblueredblue extends PathBase {
+
+    Case2redblueredblue(LinearOpMode opMode, Robot r, Coordinate startLoc) {
+        super(opMode, r, startLoc, "Case2 red/blue red/blue");
+    }
+
+    @Override
+    void run() throws InterruptedException {
+        switch (alliance) {
+            case BLUE:
+                robot.encoderDrive(-0.5, 5); //backward
+                robot.ballshooter(0.95, 1);
+                robot.feeder(0, 700); //down feed
+                robot.feeder(1, 1000); //up feed
+                robot.feeder(0, 1000); //down feed
+                robot.feeder(1, 1000); //up feed
+                robot.feeder(0, 200); //down feed
+                robot.feeder(1, 0); //up feed
+                robot.ballshooter(0, 0); //stop shooter
+                robot.pivot(56, 0.2); //pivot
+                robot.encoderDrive(-0.9, 62.2); //gets to beacon
+                robot.pivot(55, 0.2);
+                robot.encoderDrive(-0.5, 1);
+                robot.encoderDrive(-0.2, 3);
+                robot.pivot(-45, 0.2);
+                robot.pivot(45, 0.2);
+                robot.encoderDrive(-0.5, 7);
+                robot.encoderDrive(0.5, 7);
+                robot.pivot(-92, 0.2);
+                robot.encoderDrive(-0.7, 52);
+                robot.pivot(92, 0.2);
+                robot.encoderDrive(-0.2, 3);
+                robot.pivot(-45, 0.2);
+                robot.pivot(45, 0.2);
+                robot.encoderDrive(-0.5, 12);
+                robot.encoderDrive(0.5, 10);
+                robot.pivot(45, -0.3);
+                robot.encoderDrive(0.5, 50);
+                break;
+
+            case RED:
+                robot.encoderDrive(-0.5, 5); //backward
+                robot.ballshooter(0.95, 100);
+                robot.feeder(0, 700); //down feed
+                robot.feeder(1, 1000); //up feed
+                robot.feeder(0, 1000); //down feed
+                robot.feeder(1, 1000); //up feed
+                robot.feeder(0, 1000); //down feed
+                robot.feeder(1, 500); //up feed
+                robot.ballshooter(0, 0);
+                robot.pivot(-56, 0.2); //pivot
+                robot.encoderDrive(0.9, 62.2);
+                robot.pivot(-55, 0.2);
+                robot.encoderDrive(-0.5, 1);
+                robot.encoderDrive(-0.2, 3);
+                robot.pivot(45, 0.2);
+                robot.pivot(-45, 0.2);
+                robot.encoderDrive(-0.5, 7);
+                robot.encoderDrive(0.5, 7);
+                robot.pivot(92, 0.2);
+                robot.encoderDrive(0.7, 52);
+                robot.pivot(-92, 0.2);
+                robot.encoderDrive(-0.2, 3);
+                robot.pivot(45, 0.2);
+                robot.pivot(-45, 0.2);
+                robot.encoderDrive(-0.5, 12);
+                robot.encoderDrive(0.5, 10);
+                robot.pivot(45, -0.3);
+                robot.encoderDrive(0.5, 50);
+                break;
+        }
+    }
+}
